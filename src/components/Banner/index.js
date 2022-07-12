0e2f044093d091672b609b8b4d40198a44a93038
Sniim <73325410+mhd-1999@users.noreply.github.com>
2022-07-12 15:52:28 +0700

    Update index.js

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -13,7 +13,7 @@ function Banner() {
         <a href='#'>
             <div className={clsx(styles.scrollDown)}>
                 <div className={clsx(styles.mouse)}>
-                    <a href="#about"><span></span></a>
+                    <a href="#About"><span></span></a>
                 </div>
                 {/* <div className={clsx(styles.arrow)}>
                     <span></span>
